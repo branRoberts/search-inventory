adf3ccdc49cbc6f6601eca1204d94907ada82f38
branRoberts <brandonkrob12@gmail.com>
2026-04-22 12:55:02 -0400

    Third Commit I did the first bonus and put it into the getinventory method

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,10 +1,15 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class SearchInventory {
     public static void main(String[] args) {
         ArrayList<Product> inventory = getInventory();
+
 
         for (Product product : inventory) {
             System.out.println(product);
@@ -21,7 +26,22 @@ public class SearchInventory {
 //        inventory.add(new Product(3, "Cheese", 300));
 //        inventory.add(new Product(4, "Coffee", 400));
 //        inventory.add(new Product(5, "Cup", 500));
+        String file = "inventory.csv";
+        String line;
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] lineArray = line.split("\\|");
+                int id = Integer.parseInt(lineArray[0]);
+                String name = lineArray[1];
+                double price = Double.parseDouble(lineArray[2]);
+                inventory.add(new Product(id, name, price));
+            }
 
+        }catch (Exception e){
+            System.out.println("Error");
+        }
 
         return inventory;
     }
