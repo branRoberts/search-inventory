207e9935474dc8d97988b9234e4f43e5889f7da6
branRoberts <brandonkrob12@gmail.com>
2026-04-22 11:12:35 -0400

    Second Commit commented out the first step

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class SearchInventory {
+    public static void main(String[] args) {
+        ArrayList<Product> inventory = getInventory();
+
+        for (Product product : inventory) {
+            System.out.println(product);
+        }
+
+    }
+
+    public static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<>();
+
+        // Write your code here
+//        inventory.add(new Product(1, "Apple", 100));
+//        inventory.add(new Product(2, "Banana", 200));
+//        inventory.add(new Product(3, "Cheese", 300));
+//        inventory.add(new Product(4, "Coffee", 400));
+//        inventory.add(new Product(5, "Cup", 500));
+
+
+        return inventory;
+    }
 }
